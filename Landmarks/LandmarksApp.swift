c749d94a8f6c3051846149a190321e1ffaf1b2e7
Dr. Andreas Plagens <andreas@plagens.eu>
2020-12-20 21:15:02 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dr. Andreas Plagens on 20.12.20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
